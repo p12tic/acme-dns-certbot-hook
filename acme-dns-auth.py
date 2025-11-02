fd4dd18a44300bc6dd475933c5a3d3280383bf1e
PartemImperium <88595311+PartemImperium@users.noreply.github.com>
2025-11-02 15:27:35 +0200

    Add env var support

--- acme-dns-auth.py
+++ acme-dns-auth.py
@@ -9,14 +9,14 @@ import requests
 ### EDIT THESE: Configuration values ###
 
 # URL to acme-dns instance
-ACMEDNS_URL = 'https://auth.acme-dns.io'
+ACMEDNS_URL = os.environ.get('ACMEDNS_URL', 'https://auth.acme-dns.io')
 # Path for acme-dns credential storage
-STORAGE_PATH = '/etc/letsencrypt/acmedns.json'
+STORAGE_PATH = os.environ.get('STORAGE_PATH', '/etc/letsencrypt/acmedns.json')
 # Whitelist for address ranges to allow the updates from
 # Example: ALLOW_FROM = ["192.168.10.0/24", "::1/128"]
-ALLOW_FROM: list[str] = []
+ALLOW_FROM: list[str] = os.environ.get('ALLOW_FROM', [])
 # Force re-registration. Overwrites the already existing acme-dns accounts.
-FORCE_REGISTER = False
+FORCE_REGISTER = os.environ.get('FORCE_REGISTER', False)
 
 ###   DO NOT EDIT BELOW THIS POINT   ###
 ###         HERE BE DRAGONS          ###
