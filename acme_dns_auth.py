377fa868e8af9525d18bfae0cf760700297c75b4
Povilas Kanapickas <povilas@radix.lt>
2025-11-02 15:27:43 +0200

    Improve errors when env variables contain bad config

--- acme_dns_auth.py
+++ acme_dns_auth.py
@@ -21,28 +21,28 @@ class AcmeDnsConfig:
 def build_acme_dns_config_from_env(config_file_path: str) -> AcmeDnsConfig:
     acmedns_url = os.environ.get('ACMEDNS_URL')
 
-    if acmedns_url is not None:
-        return AcmeDnsConfig(
-            url=acmedns_url,
-            allow_from=json.loads(os.environ.get('ACMEDNS_ALLOW_FROM', '[]')),
-            force_register=json.loads(os.environ.get('ACMEDNS_FORCE_REGISTER', 'false')),
-        )
-    else:
-        try:
-            with open(config_file_path) as f:
-                config_data = json.load(f)
-
+    try:
+        if acmedns_url is not None:
             return AcmeDnsConfig(
-                url=config_data.get('url'),
-                allow_from=config_data.get('allow_from', []),
-                force_register=config_data.get('force_register', False),
+                url=acmedns_url,
+                allow_from=json.loads(os.environ.get('ACMEDNS_ALLOW_FROM', '[]')),
+                force_register=json.loads(os.environ.get('ACMEDNS_FORCE_REGISTER', 'false')),
             )
-        except FileNotFoundError:
-            raise RuntimeError(
-                'ERROR: No configuration supplied either via environment variables nor config file'
-            )
-        except (json.JSONDecodeError, KeyError) as e:
-            raise RuntimeError(f'ERROR: Invalid configuration in {config_file_path}: {str(e)}')
+
+        with open(config_file_path) as f:
+            config_data = json.load(f)
+
+        return AcmeDnsConfig(
+            url=config_data.get('url'),
+            allow_from=config_data.get('allow_from', []),
+            force_register=config_data.get('force_register', False),
+        )
+    except FileNotFoundError:
+        raise RuntimeError(
+            'ERROR: No configuration supplied either via environment variables nor config file'
+        )
+    except (json.JSONDecodeError, KeyError) as e:
+        raise RuntimeError(f'ERROR: Invalid configuration in {config_file_path}: {str(e)}')
 
 
 class AcmeDnsClient:
